9ace53e0a4bbecbaff3a100f5140b2575b2b690b
SonyaMarmeladova <7exalted7@gmail.com>
2019-06-14 14:18:15 +0300

    add basic table to analysis

--- src/app/core/components/analysis/analysis.component.ts
+++ src/app/core/components/analysis/analysis.component.ts
@@ -1,18 +1,60 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { merge, Observable, of as observableOf } from 'rxjs';
+import { catchError, map, startWith, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-analysis',
   templateUrl: './analysis.component.html',
   styleUrls: ['./analysis.component.scss']
 })
-export class AnalysisComponent implements OnInit {
+export class AnalysisComponent implements OnInit, AfterViewInit {
+
+  displayedColumns: string[] = ['n'];
+  data = [];
+
+  resultsLength = 0;
+  isLoadingResults = true;
+  isRateLimitReached = false;
 
   constructor(private httpClient: HttpClient) { }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
   ngOnInit() {
-    this.httpClient.get('/capi/ChikiBambuki/Openings').subscribe(data => {
-      console.log(data);
-    });
+    // this.httpClient.get('/capi/ChikiBambuki/Openings?sort=n').subscribe(data => {
+    //   console.log(data);
+    // });
+  }
+
+  ngAfterViewInit() {
+      // If the user changes the sort order, reset back to the first page.
+      // this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+
+      merge(this.paginator.page)
+      .pipe(
+        startWith({}),
+        switchMap(() => {
+          this.isLoadingResults = true;
+          return this.httpClient.get('/capi/ChikiBambuki/Openings?sort=n');
+        }),
+        map((data: any) => {
+          // Flip flag to show that loading has finished.
+          this.isLoadingResults = false;
+          this.isRateLimitReached = false;
+          this.resultsLength = data._returned;
+
+          return data._embedded;
+        }),
+        catchError(() => {
+          this.isLoadingResults = false;
+          // Catch if the GitHub API has reached its rate limit. Return empty data.
+          this.isRateLimitReached = true;
+          return observableOf([]);
+        })
+      ).subscribe(data => this.data = data);
   }
 }
