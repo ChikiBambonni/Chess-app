e851d460913ff41d983d1bf71d35cc4db9bab1c6
SonyaMarmeladova <7exalted7@gmail.com>
2019-06-20 14:09:48 +0300

    create mock service

---
+++ src/app/core/mock-backend/mock-backend.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+
+import { MockBackendOption } from './mock-backend.interfaces';
+import { StorageService } from '@core/services/storage/storage.service';
+import { BackendDataMode } from './mock-backend.enum';
+import { MockBackendUrl } from './mock-backend-url.class';
+
+import * as UrlPattern from 'url-pattern';
+
+@Injectable()
+export class MockBackendService {
+  public static options: Map<string, MockBackendOption>;
+
+  public static keys: any;
+
+  private readonly key = 'mock_data_service_v1';
+  private mode: BackendDataMode = BackendDataMode.Mock;
+  private defaultMode: BackendDataMode = BackendDataMode.Mock;
+
+  constructor() {
+    this.setMode(StorageService.getItem(this.key) || this.defaultMode);
+  }
+
+  public static initConfig(configUrl: MockBackendUrl): void {
+    const options = configUrl.getConfig();
+
+    this.keys = Object.keys(options).map(key => ({
+      path: key,
+      pattern: new UrlPattern(key),
+      mock: options[key]
+    }));
+
+    this.options = configUrl.createMap(options);
+  }
+
+  public initGlobalMethods(): void {
+    (window as any).setMockMode = () => {
+      this.setMode(BackendDataMode.Mock, true);
+    };
+
+    (window as any).setRealMode = () => {
+      this.setMode(BackendDataMode.Real, true);
+    };
+  }
+
+  public getMode(): BackendDataMode {
+    return this.mode;
+  }
+
+  public setMode(mode: BackendDataMode, reload: boolean = false): void {
+    this.mode = mode;
+    this.changeMode(mode, reload);
+  }
+
+  public isMock(): boolean {
+    return this.mode === BackendDataMode.Mock;
+  }
+
+  private changeMode(mode: BackendDataMode, reload: boolean): void {
+    StorageService.setItem(this.key, mode.toString());
+
+    if (reload) {
+      window.location.reload();
+    }
+  }
+}
--- src/app/core/services/storage/storage.service.ts
+++ src/app/core/services/storage/storage.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { Safe } from '@core/decorators/safe.decorator';
+
+@Injectable()
 export class StorageService {
 
-  constructor() { }
+  @Safe()
+  public static getItem<T>(key: string): T {
+    return this.fromJson(sessionStorage.getItem(key)) || this.fromJson(localStorage.getItem(key));
+  }
+
+  @Safe()
+  public static setItem<T>(key: string, valueObj: T, remember: boolean = true) {
+    if (remember) {
+      localStorage.setItem(key, this.toJson<T>(valueObj));
+    } else {
+      sessionStorage.setItem(key, this.toJson<T>(valueObj));
+    }
+  }
+
+  @Safe()
+  public static removeItem(key: string) {
+    sessionStorage.removeItem(key);
+    localStorage .removeItem(key);
+  }
+
+  private static fromJson<T>(value: string): T {
+    return JSON.parse(value);
+  }
+
+  private static toJson<T>(value: T): string {
+    return JSON.stringify(value);
+  }
 }
