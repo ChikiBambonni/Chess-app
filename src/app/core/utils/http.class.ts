afc5fbd75d61ddb5ae74034396c0bce296feaf88
SonyaMarmeladova <7exalted7@gmail.com>
2019-06-23 16:39:40 +0300

    fix params

--- src/app/core/utils/http.class.ts
+++ src/app/core/utils/http.class.ts
@@ -8,6 +8,12 @@ export abstract class Http {
   protected getRequestParams(params: object | null): HttpParams {
     let requestParams: HttpParams = new HttpParams();
 
+    if (params) {
+      Object.keys(params)
+        .filter((key: string) => params[key] !== undefined && params[key] !== null)
+        .forEach((key: string) => requestParams = this.updateParams(requestParams, key, params[key]));
+    }
+
     return requestParams;
   }
 
