7f1f4b185c62a7884180b69bd380c2825a0e18ef
SonyaMarmeladova <7exalted7@gmail.com>
2019-06-26 16:43:55 +0300

    fix promotion choice

--- src/app/shared/components/chessground/chessground.module.ts
+++ src/app/shared/components/chessground/chessground.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PromotionChoiceModule } from '@shared/components/promotion-choice/promotion-choice.module';
+import { PromotionChoiceComponent } from '@shared/components/promotion-choice/promotion-choice.component';
 import { ChessgroundComponent } from './chessground.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    PromotionChoiceModule
   ],
   declarations: [
     ChessgroundComponent
   ],
   exports: [
     ChessgroundComponent
+  ],
+  entryComponents: [
+    PromotionChoiceComponent
   ]
 })
 export class ChessgroundModule { }
