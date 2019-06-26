272b8ed59e00ecbb66d1a73a72268b1e770129a6
SonyaMarmeladova <7exalted7@gmail.com>
2019-06-26 12:47:18 +0300

    clean up in core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,23 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import { MaterialModule } from '@material/material.module';
 import { ErrorInterceptor } from '@core/mock-backend/interceptors/error.interceptor';
 import { JwtInterceptor } from '@core/mock-backend/interceptors/jwt.interceptor';
 import { fakeBackendProvider } from '@core/mock-backend/interceptors/auth.interceptor';
-
+import { AuthenticationService } from '@core/mock-backend/services/auth.service';
 import { MessageService } from '@core/services/message/message.service';
 import { ChatService } from '@core/services/chat/chat.service';
 import { UserService } from '@core/mock-backend/services/user.service';
-import { AuthenticationService } from '@core/mock-backend/services/auth.service';
 import { WebsocketService } from '@core/services/websocket/websocket.service';
-
 import { ChessGameService } from '@core/services/chess-game/chess-game.service';
-
-
 import { FindGameDialogComponent } from '@core/material-dialogs/find-game-dialog/find-game-dialog.component';
-import { MaterialModule } from '@material/material.module';
-
-
 import { MockDataInterceptor } from './mock-backend/mock-data.interceptor';
 import { MockBackendService } from './mock-backend/mock-backend.service';
 import { MockBackendConfig } from './mock-backend/mock-backend-config.constant';
