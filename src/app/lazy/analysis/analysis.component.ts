4c63dbdf9320955d8eab7c0cf525ce880fee12f1
SonyaMarmeladova <7exalted7@gmail.com>
2019-08-26 12:11:48 +0300

    move worker logic to analysis component

--- src/app/lazy/analysis/analysis.component.ts
+++ src/app/lazy/analysis/analysis.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as _ from 'lodash';
 
+import { WorkerService } from '../../app-worker.service';
 import { ChessMove, CgMove } from '@core/interfaces/chess-move.interfaces';
 import { pushMove } from '@core/utils/chess.utils';
 import { AnalysisService } from './analysis.service';
@@ -16,11 +17,20 @@ export class AnalysisComponent implements OnInit {
   fen: string = null;
   m: string = null;
   opening = 'Custom Variation';
+  score = 0;
   data: ChessMove[] = [{ N: 1 }];
 
-  constructor() { }
+  constructor(private workerService: WorkerService) { }
+
+  private setM() {
+    this.workerService.postMessage(`position startpos moves ${this.m}`);
+    this.workerService.postMessage('go depth 1 wtime 300000 winc 2000 btime 300000 binc 2000');
+  }
 
   ngOnInit() {
+    this.workerService.workerUpdate$.subscribe(line => {
+      this.score = Number(this.workerService.engineStatus.score);
+    });
   }
 
   changeFEN($event: Opening) {
@@ -28,9 +38,12 @@ export class AnalysisComponent implements OnInit {
     this.fen = $event.f;
     this.opening = $event.n;
     this.m = $event.m;
+    this.setM();
   }
 
   onMove($event: CgMove) {
     this.data = pushMove(this.data, $event);
+    this.setM();
+    console.log('Move', $event, this.m);
   }
 }
--- src/app/lazy/analysis/components/openings-mt/openings-mt.component.ts
+++ src/app/lazy/analysis/components/openings-mt/openings-mt.component.ts
@@ -2,25 +2,22 @@ import {
   Component,
   OnInit,
   Input,
-  OnChanges,
-  SimpleChanges
 } from '@angular/core';
 
-import { WorkerService } from '../../../../app-worker.service';
 import { ChessMove } from '@core/interfaces/chess-move.interfaces';
-import { TrackChanges } from '@core/decorators/changes.decorator';
-import { ChangesStrategy } from '@core/enums/changes-strategy.emuns';
 
 @Component({
   selector: 'app-openings-mt',
   templateUrl: './openings-mt.component.html',
   styleUrls: ['./openings-mt.component.scss']
 })
-export class OpeningsMtComponent implements OnInit, OnChanges {
+export class OpeningsMtComponent implements OnInit {
 
   min = -20;
   max = 20;
-  score = 0;
+
+  @Input()
+  score: number;
 
   @Input()
   data: ChessMove[];
@@ -28,20 +25,8 @@ export class OpeningsMtComponent implements OnInit, OnChanges {
   @Input()
   m: string;
 
-  constructor(private workerService: WorkerService) { }
-
-  private setM() {
-    this.workerService.postMessage(`position startpos moves ${this.m}`);
-    this.workerService.postMessage('go depth 1 wtime 300000 winc 2000 btime 300000 binc 2000');
-  }
+  constructor() { }
 
   ngOnInit() {
-    this.workerService.workerUpdate$.subscribe(line => {
-      this.score = Number(this.workerService.engineStatus.score);
-    });
-  }
-
-  @TrackChanges('m', 'setM', ChangesStrategy.NonFirst)
-  ngOnChanges(changes: SimpleChanges) {
   }
 }
