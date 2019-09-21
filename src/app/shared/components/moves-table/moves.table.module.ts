4b58bbad0be83803f01810ffde5e0a803ac2b8e7
SonyaMarmeladova <7exalted7@gmail.com>
2019-09-21 14:06:05 +0300

    add arrow icons

--- src/app/shared/components/moves-table/moves.table.module.ts
+++ src/app/shared/components/moves-table/moves.table.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CommonIconModule } from '@shared/components/common-icon/common-icon.module';
 import { CommonTableModule } from '@shared/components/common-table/common-table.module';
 import { MovesTableComponent } from './moves-table.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    CommonTableModule
+    CommonTableModule,
+    CommonIconModule
   ],
   declarations: [
     MovesTableComponent
