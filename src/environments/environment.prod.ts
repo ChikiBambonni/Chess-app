29a53f0f56989a010dd0ee175d2f6c642a4d6610
SonyaMarmeladova <7exalted7@gmail.com>
2019-11-25 18:05:44 +0200

    change login form autocomplete

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: true,
+  ws_uri: 'http://localhost:3000',
+  socket_config: {
+    'reconnection': true,
+    'reconnectionDelay': 1000,
+    'reconnectionDelayMax' : 5000,
+    'reconnectionAttempts': 5
+  },
   mongoapi: '/mapi',
 };
